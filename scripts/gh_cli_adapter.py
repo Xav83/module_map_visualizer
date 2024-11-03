429fdba45837dc4eed8a82f012b69364aa2064ae
Xavier Jouvenot <x.jouvenot@expressivee.com>
2024-11-03 17:50:47 +0100

    Silences the irrelevant bandit warnings from gh_cli_adapter.py

--- scripts/gh_cli_adapter.py
+++ scripts/gh_cli_adapter.py
@@ -1,7 +1,7 @@
 import json
 import os
 import shlex
-import subprocess
+import subprocess  # nosec B404
 import time
 
 
@@ -10,7 +10,7 @@ class GhCliAdapter:
     def get_rate_limit_info():
         cmd = "gh api rate_limit"
         return json.loads(
-            subprocess.run(
+            subprocess.run(  # nosec B603
                 shlex.split(cmd), capture_output=True, text=True, check=True
             ).stdout
         )
@@ -37,7 +37,7 @@ class GhCliAdapter:
                 time.sleep(1)
         cmd = f"gh search code {code_to_search} {f'--repo {owner}/{repository}' if repository is not None else f'--owner {owner}'} {f'--filename {filename}' if filename is not None else ''} --match {'path' if matching_filename else 'file'} --json path,repository,textMatches --limit 999"
         return json.loads(
-            subprocess.run(
+            subprocess.run(  # nosec B603
                 shlex.split(cmd), capture_output=True, text=True, check=True
             ).stdout
         )
@@ -52,7 +52,7 @@ class GhCliAdapter:
                 time.sleep(1)
         cmd = f"gh search prs --owner {repository_owner} --state {pr_state} --head {branch_name} --archived=false --json number,repository"
         return json.loads(
-            subprocess.run(
+            subprocess.run(  # nosec B603
                 shlex.split(cmd), capture_output=True, text=True, check=True
             ).stdout
         )
@@ -60,25 +60,25 @@ class GhCliAdapter:
     @staticmethod
     def pr_new_comment(pull_request_number, owner, repository, comment_message):
         cmd = f"gh pr comment {pull_request_number} -b '{comment_message}' -R {owner}/{repository}"
-        subprocess.run(shlex.split(cmd), check=True)
+        subprocess.run(shlex.split(cmd), check=True)  # nosec B603
 
     @staticmethod
     def pr_approve(pull_request_number, owner, repository):
         cmd = (
             f"gh pr review {pull_request_number} --approve --repo {owner}/{repository}"
         )
-        subprocess.run(shlex.split(cmd), check=True)
+        subprocess.run(shlex.split(cmd), check=True)  # nosec B603
 
     @staticmethod
     def pr_merge(pull_request_number, owner, repository):
         cmd = f"gh pr merge {pull_request_number} --merge --delete-branch --repo {owner}/{repository}"
-        subprocess.run(shlex.split(cmd), check=True)
+        subprocess.run(shlex.split(cmd), check=True)  # nosec B603
 
     @staticmethod
     def pr_view(pull_request_number, owner, repository):
         cmd = f"gh pr view {pull_request_number} --json author,baseRefName,comments,commits,mergeable,mergeStateStatus,number,reviewDecision,reviews,state,statusCheckRollup,url -R {owner}/{repository}"
         return json.loads(
-            subprocess.run(
+            subprocess.run(  # nosec B603
                 shlex.split(cmd), capture_output=True, text=True, check=True
             ).stdout
         )
@@ -86,14 +86,14 @@ class GhCliAdapter:
     @staticmethod
     def pr_rebase(pull_request_number, owner, repository):
         cmd = f"gh pr update-branch {pull_request_number} --rebase -R {owner}/{repository}"
-        subprocess.run(shlex.split(cmd), check=True)
+        subprocess.run(shlex.split(cmd), check=True)  # nosec B603
 
     @staticmethod
     def pr_checkout(pull_request_number, repository_location):
         if not os.path.exists(repository_location):
             raise OSError(f"No git repository found in {repository_location}")
         cmd = f"gh pr checkout {pull_request_number}"
-        subprocess.run(
+        subprocess.run(  # nosec B603
             shlex.split(cmd),
             cwd=repository_location,
             stdout=subprocess.DEVNULL,
@@ -105,7 +105,7 @@ class GhCliAdapter:
         if not os.path.exists(repository_location):
             raise OSError(f"No git repository found in {repository_location}")
         cmd = f"gh pr create --title {pull_request_title} --body {pull_request_description}"
-        return subprocess.run(
+        return subprocess.run(  # nosec B603
             shlex.split(cmd),
             cwd=repository_location,
             capture_output=True,
@@ -118,10 +118,14 @@ class GhCliAdapter:
         if os.path.exists(repository):
             raise OSError(f"Git repository ' {repository}' already existing.")
         cmd = f"gh repo clone {owner}/{repository}"
-        subprocess.run(shlex.split(cmd), stdout=subprocess.DEVNULL, check=True)
+        subprocess.run(
+            shlex.split(cmd), stdout=subprocess.DEVNULL, check=True
+        )  # nosec B603
 
     # https://github.com/mislav/gh-cp
     @staticmethod
     def download_file(owner, repository, file, file_destination="."):
         cmd = f"gh cp {owner}/{repository} {file} {file_destination}"
-        subprocess.run(shlex.split(cmd), stdout=subprocess.DEVNULL, check=True)
+        subprocess.run(
+            shlex.split(cmd), stdout=subprocess.DEVNULL, check=True
+        )  # nosec B603
